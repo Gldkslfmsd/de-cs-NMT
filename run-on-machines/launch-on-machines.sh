63b8f76cb7b928adba8f1bb741f7085199827a78
Dominik Macháček <gldkslfmsd@gmail.com>
2017-09-30 16:48:15 +0200

    next results

--- run-on-machines/launch-on-machines.sh
+++ run-on-machines/launch-on-machines.sh
@@ -1,6 +1,8 @@
-QS=th preprocess.lua -train_src data/src-train.txt -train_tgt data/tgt-train.txt -valid_src data/src-val.txt -valid_tgt data/tgt-val.txt -save_data data/demo
+QS="th preprocess.lua -train_src data/src-train.txt -train_tgt data/tgt-train.txt -valid_src data/src-val.txt -valid_tgt data/tgt-val.txt -save_data data/demo"
 : > ok
-cat 30ips | while read us ip; do 
+cat 10ips-dm |
+#echo bojar 13.65.37.251 -p 50010 | 
+while read us ip; do 
 		echo $us $ip; 
 #		echo for s in \`find .\`\; do sudo chown $us \$s\; done  |
 #		echo luarocks install tds |
@@ -8,15 +10,24 @@ cat 30ips | while read us ip; do
 #		echo rm -rf OpenNMT/*.t7 |
 #		echo cd OpenNMT \; th preprocess.lua -train_src data/src-train.txt -train_tgt data/tgt-train.txt -valid_src data/src-val.txt -valid_tgt data/tgt-val.txt -save_data data/demo |
 #		echo cd OpenNMT \&\& th preprocess.lua -train_src \~/csen-data/train.bpe.cs -train_tgt \~/csen-data/train.bpe.en  -valid_src \~/csen-data/dev.bpe.cs -valid_tgt \~/csen-data/dev.bpe.en -save_data /mnt/csen-onmt-data  |
- 	#	echo rm -rf nvidia *out *log *pid *.err \; |
-		#echo nohup ./launch-script.sh \< par \& |
-		echo nohup ./launch-script.sh \< par \& |
-	#	echo rm -rf *.err *.pid *.log |
+# 		echo rm -rf nvidia *out *log *pid *.err \; cd OpenNMT \; rm -rf nvidia *out *log *pid *err *pid# |
+#		echo rm -rf abc cmd lsonmt* |
+#		echo "nohup [ ! -s par.running ] && ./launch-script.sh par &" |
+		echo "nohup ./launch-script.sh par &" |
+	#	echo cp /share/obo-machacek/install-onmt.sh . \; ./install-onmt.sh |
+#		echo cp /share/obo-machacek/install-lua5.2.sh . \; ./install-lua5.2.sh |
+
+#		echo luarocks install tds |
+#		echo kill \`cat *.pid\`\; |
+#		echo rm -rf *.err *.pid *.log *.out |
 #		echo mkdir data-50k \; cp /share/obo-machacek/data-50k-csen/* data-50k |
 #		echo chmod -x data-50k/* |
 	
+#		echo ps aux \| grep $us |
 #		echo kill -9 \`cat *.pid\` |
 #		echo ls -la OpenNMT* \> lsonmt\$\(hostname\) |
+
+#		echo kill "\`ps aux | grep $us | grep train.lua | sed 's/ \+/ /g' | cut -f 2 -d\" \"\`" |
 		ssh $us@$ip && echo $us $ip  >> ok || echo ko $us $ip >> ok &
 done
 wait
--- run-on-machines/launch-script.sh
+++ run-on-machines/launch-script.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 # < params
+
+par=$PWD/$1
 cd OpenNMT
 i=1
 HOST=`hostname`
-while read line; do
-	suf=`echo $line | sed 's/ /_/g'`
-	echo
-	echo
-	echo $suf 
-	echo
-	echo
-	echo
-	echo $line #-log_file $HOME/$HOST""-$i-$suf"".log
+	echo tady $par
+	cat $par
+: > $par"".finished
+while [ -s "$par" ]; do
+	cat $par
+	echo tady $par
+	CMD=`head -n 1 "$par"`
+	tmp=`mktemp`
+	tail -n +2 "$par" > $tmp
+	mv $tmp "$par"
+	echo $CMD > $par"".running
+
+	echo $CMD #-log_file $HOME/$HOST""-$i-$suf"".log
 	export THC_CACHING_ALLOCATOR=0
-	eval "$line > ~/`hostname`-$i"".out 2>`hostname`-$i"".err" 
+	eval "$CMD" & #> ~/`hostname`-$i"".out 2> ~/`hostname`-$i"".err" &
 	echo $!  > ~/$i"".pid # $HOME/$HOST""-$i-$suf"".pid
+
+
 
 #	for i in `seq 10`; do
 #		nvidia-smi > ~/nvidia
 #		sleep 1
 #	done
 	wait
+
+	echo $CMD >> $par"".finished
+	: > $par"".running
 
 	i=$(($i+1))
 done
