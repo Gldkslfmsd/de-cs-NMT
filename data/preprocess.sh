1604422ff83a6d83fd5fe0ede45a1cd42731517f
Dominik Macháček <gldkslfmsd@gmail.com>
2017-10-16 09:02:45 +0200

    report first part

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -1,4 +1,7 @@
-MOSES=moses/scripts/tokenizer
+if [ ! -z "$1" ]; then
+	BASE="$1/"
+fi
+MOSES=$BASE""moses/scripts/tokenizer
 		perl $MOSES/remove-non-printing-char.perl | \
 		perl $MOSES/replace-unicode-punctuation.perl | \
 		perl $MOSES/normalize-punctuation.perl 
