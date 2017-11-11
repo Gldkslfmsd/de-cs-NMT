5c4c6d8d960b561f6f576dadf2551ca5c7af281c
Dominik Macháček <gldkslfmsd@gmail.com>
2017-11-11 23:06:47 +0100

    cu-depfix do reportu

--- final_data/results/sample100.txt.notes2.cmd
+++ final_data/results/sample100.txt.notes2.cmd
@@ -1,1 +1,1 @@
-./anotace3.py ../../test.de.post ../../test.cs.post zeman.cs.pred google.cs.tok ../../chimera-2013/chimera.cs.pred model-osub-europarl50000-2_checkpoint.t72017-10-26T05:36:43Z.cs.pred.post notes
+./anotace3.py ../../test.de.post ../../test.cs.post zeman.cs.pred google.cs.tok ../../chimera-2013/chimera.cs.pred model-osub-europarl50000-2_checkpoint.t72017-10-26T05:36:43Z.cs.pred.post notes > sample100.txt.notes2
